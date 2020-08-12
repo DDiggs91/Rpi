9a9fe29cb98efdab9b00bea60f08efdea046ab19
Daniel Diggs <danielsdiggs@gmail.com>
2020-08-12 23:27:00 +0100

    displaying text

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ FONT = pygame.font.Font(None, 25)
 while True:
     event_upkeep = []
     for event in pygame.event.get():
-        event_upkeep.append(event)
+        event_upkeep.append(str(event))
 
     DISPLAY.fill((255, 0, 0))
 
-    text = FONT.render(' '.join(event_upkeep), True, (0, 0, 0))
+    text = FONT.render(','.join(event_upkeep), True, (0, 0, 0))
     DISPLAY.blit(text, (250, 250))
 
     text2 = FONT.render("{}".format(pygame.display.get_surface().get_size()), True, (0, 0, 0))
     DISPLAY.blit(text2, (350, 350))
-    pygame.display.flip()
+    pygame.display.update()
     FPSCLOCK.tick(1)

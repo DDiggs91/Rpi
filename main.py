7edaa825e775d25a8651d85ffbf037c118ff1ee6
Daniel Diggs <danielsdiggs@gmail.com>
2020-08-12 22:22:53 +0100

    added basic window to test

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pygame
 
-#Window 800x600
+# Window 800x600
+screen = (800, 600)
+pygame.init()
+DISPLAY = pygame.display.set_mode(screen)
+FPSCLOCK = pygame.time.Clock()
+while True:
+    DISPLAY.fill((255, 0, 0))
+    pygame.display.flip()
+    FPSCLOCK.tick()

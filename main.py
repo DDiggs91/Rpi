5a692fc3b20a1b8accc985b94d9f6c7c928f3f08
Daniel Diggs <danielsdiggs@gmail.com>
2020-08-13 00:27:16 +0100

    displaying text

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ while True:
     event_upkeep = []
     for event in pygame.event.get():
         if event.type == pygame.MOUSEMOTION:
-            mouse_pos = [str(i) for i in event.pos}
+            mouse_pos = [str(i) for i in event.pos]
 
     DISPLAY.fill((255, 0, 0))
 
     text = BIGFONT.render(','.join(mouse_pos), True, (0, 0, 0))
-    DISPLAY.blit(text, (350, 350))
+    DISPLAY.blit(text, (WINDOWWIDTH // 2, WINDOWHEIGHT // 2))
 
     pygame.display.update()
     FPSCLOCK.tick(60)

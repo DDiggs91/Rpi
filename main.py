90c8040587a043918fe74db9c71ba2640fe7f0fc
Daniel Diggs <danielsdiggs@gmail.com>
2020-08-12 22:55:14 +0100

    changed format to fullscreen

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 # Window 800x600
 screen = (800, 600)
 pygame.init()
-DISPLAY = pygame.display.set_mode(screen)
+DISPLAY = pygame.display.set_mode(screen, pygame.FULLSCREEN)
 FPSCLOCK = pygame.time.Clock()
 while True:
     DISPLAY.fill((255, 0, 0))

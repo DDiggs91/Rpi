67e0136f8e713b7ad9a9f5ffed6a417472e5be99
Daniel Diggs <danielsdiggs@gmail.com>
2020-08-13 00:23:53 +0100

    displaying text

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import pygame
 # Window 800x480
 pygame.init()
 DISPLAY = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+WINDOWWIDTH, WINDOWHEIGHT = pygame.display.get_surface().get_size()
 FPSCLOCK = pygame.time.Clock()
 BIGFONT = pygame.font.SysFont(None, 80)
 SMALLFONT = pygame.font.SysFont(None, 40)
+
+mouse_pos = (0, 0)
+
 while True:
     event_upkeep = []
     for event in pygame.event.get():
-        event_upkeep.append(str(event))
+        if event.type == pygame.MOUSEMOTION:
+            mouse_pos = event.pos
 
     DISPLAY.fill((255, 0, 0))
 
-    text = BIGFONT.render("{}".format(pygame.display.get_surface().get_size()), True, (0, 0, 0))
+    text = BIGFONT.render(','.format(mouse_pos), True, (0, 0, 0))
     DISPLAY.blit(text, (350, 350))
-
-    text = SMALLFONT.render("{}".format(pygame.display.get_surface().get_size()), True, (0, 0, 0))
-    DISPLAY.blit(text, (450, 450))
 
     pygame.display.update()
     FPSCLOCK.tick(60)

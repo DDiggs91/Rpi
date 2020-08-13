b7cb91c35714c44581dca07e738e3a2dbbc7b121
Daniel Diggs <danielsdiggs@gmail.com>
2020-08-13 13:56:22 +0100

    Fixed formatting Allowed system selected display

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import pygame
-import platform
+from platform import system
 
-# Window 800x480
-pygame.init()
-print(platform.system())
-quit()
-if platform.system() == 'Windows':
-    DISPLAY = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-elif platform.system() ==
-    DISPLAY = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-WINDOWWIDTH, WINDOWHEIGHT = pygame.display.get_surface().get_size()
-FPSCLOCK = pygame.time.Clock()
-BIGFONT = pygame.font.SysFont(None, 80)
-SMALLFONT = pygame.font.SysFont(None, 40)
 
-mouse_pos = (0, 0)
+def main():
+    pygame.init()
+    if system() == 'Linux':
+        DISPLAY = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        SCALEFACTOR = 1
+    else:  # system() == 'Windows':
+        DISPLAY = pygame.display.set_mode((480, 270))
+        SCALEFACTOR = .25
+    WINDOWWIDTH, WINDOWHEIGHT = pygame.display.get_surface().get_size()
+    FPSCLOCK = pygame.time.Clock()
+    BIGFONT = pygame.font.SysFont(None, int(80 * SCALEFACTOR))
+    SMALLFONT = pygame.font.SysFont(None, int(40 * SCALEFACTOR))
 
-while True:
-    event_upkeep = []
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEMOTION:
-            mouse_pos = [str(i) for i in event.pos]
+    mouse_pos = (0, 0)
 
-    DISPLAY.fill((255, 0, 0))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEMOTION:
+                mouse_pos = event.pos
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-    text = BIGFONT.render(','.join(mouse_pos), True, (0, 0, 0))
-    DISPLAY.blit(text, (WINDOWWIDTH // 2, WINDOWHEIGHT // 2))
+        DISPLAY.fill((255, 0, 0))
 
-    pygame.display.update()
-    FPSCLOCK.tick(60)
+        pygame.display.update()
+        FPSCLOCK.tick(60)
+
+
+if __name__ == '__main__':
+    main()

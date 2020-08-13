73cc9b42829f828d45928179aa39ce7dd20cf0c0
Daniel Diggs <danielsdiggs@gmail.com>
2020-08-13 15:30:15 +0100

    Allowed for clicking

--- main.py
+++ main.py
@@ -6,21 +6,19 @@ def main():
     pygame.init()
     if system() == 'Linux':
         DISPLAY = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        SCALEFACTOR = 1
     else:  # system() == 'Windows':
         DISPLAY = pygame.display.set_mode((480, 270))
-        SCALEFACTOR = .25
     WINDOWWIDTH, WINDOWHEIGHT = pygame.display.get_surface().get_size()
     FPSCLOCK = pygame.time.Clock()
-    BIGFONT = pygame.font.SysFont(None, int(80 * SCALEFACTOR))
-    SMALLFONT = pygame.font.SysFont(None, int(40 * SCALEFACTOR))
+    BIGFONT = pygame.font.SysFont(None, min(WINDOWWIDTH // 6, 80))
+    SMALLFONT = pygame.font.SysFont(None, max(WINDOWWIDTH // 12, 40))
 
     mos_pos = [0, 0]
     mos_down = False
 
-    slider1 = Slider(WINDOWWIDTH * 1 // 16, WINDOWHEIGHT * 6 // 8, WINDOWWIDTH * 2 // 8, WINDOWHEIGHT * 1 // 8)
-    all_sprites = pygame.sprite.Group()
-    all_sprites.add(slider1)
+    slider1 = Slider(WINDOWWIDTH * 1 // 16, WINDOWHEIGHT * 6 // 8, WINDOWWIDTH * 2 // 8, WINDOWHEIGHT * 1 // 8, 32)
+    all_sliders = pygame.sprite.Group()
+    all_sliders.add(slider1)
 
     while True:
         for event in pygame.event.get():
@@ -28,17 +26,19 @@ def main():
                 mos_pos = event.pos
             elif event.type == pygame.MOUSEBUTTONDOWN and not mos_down:
                 mos_down = True
-                for sprite in all_sprites:
+                for sprite in all_sliders:
                     sprite.clicked(mos_pos)
             elif event.type == pygame.MOUSEBUTTONUP and mos_down:
                 mos_down = False
+                for sprite in all_sliders:
+                    sprite.selected = False
             elif event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
         DISPLAY.fill((255, 0, 0))
-        all_sprites.draw(DISPLAY)
-        all_sprites.update(mos_pos, mos_down)
+        all_sliders.draw(DISPLAY)
+        all_sliders.update(mos_pos, mos_down)
         pygame.display.update()
         FPSCLOCK.tick(60)
 
@@ -50,26 +50,40 @@ class SliderButton(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.image.fill((120, 120, 120))
 
-    def update(self, mos_pos, mos_down):
-        pass
-
 
 class Slider(pygame.sprite.Sprite):
-    def __init__(self, width, height, x, y):
+    def __init__(self, width, height, x, y, divisions):
         super().__init__()
-        self.image = pygame.Surface([width, height])
+        self.image = pygame.Surface([round(width / divisions) * divisions, round(height / divisions) * divisions])
         self.image.fill((192, 192, 192))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
-        pygame.draw.line(self.image, (0, 0, 0), (self.rect.width // 2, self.rect.height * 1 // 16),
-                         (self.rect.width // 2, self.rect.height * 15 // 16), self.rect.width * 1 // 16)
-        self.button = SliderButton(self.rect.width, self.rect.height * 1 // 16)
-        self.button.rect.topleft = (0, self.rect.height * 7 // 16)
+        self.div = divisions
+        pygame.draw.line(self.image, (0, 0, 0), (self.rect.width // 2, self.rect.height * 1 // self.div),
+                         (self.rect.width // 2, self.rect.height * (self.div - 1) // self.div),
+                         max(self.rect.width * 1 // self.div, 1))
+        self.button = SliderButton(self.rect.width, self.rect.height * 1 // self.div)
+        self.button.rect.topleft = (0, self.rect.height * (self.div // 2 - 1) // 16)
         self.image.blit(self.button.image, self.button.rect.topleft)
         self.selected = False
 
     def update(self, mos_pos, mos_down):
-        self.button.update(mos_pos, mos_down)
+        if self.selected and mos_down:
+            self.button.rect.centery = \
+                sorted([1, (mos_pos[1] - self.rect.y) // (self.rect.height // self.div), self.div - 1])[1] * (
+                        self.rect.height // self.div)
+            self.button.update()
+
+        self.image.fill((192, 192, 192))
+        pygame.draw.line(self.image, (0, 0, 0), (self.rect.width // 2, self.rect.height * 1 // self.div),
+                         (self.rect.width // 2, self.rect.height * (self.div - 1) // self.div),
+                         max(self.rect.width * 1 // self.div, 1))
+        for i in range(1, self.div):
+            pygame.draw.line(self.image, (0, 0, 0), (
+                self.rect.width * (self.div // 4 - 1) // (self.div // 2), i * self.rect.height // self.div),
+                             (self.rect.width * (self.div // 4 + 1) // (self.div // 2),
+                              i * self.rect.height // self.div))
+        self.image.blit(self.button.image, self.button.rect.topleft)
 
     def clicked(self, mos_pos):
         if self.rect.x + self.button.rect.x < mos_pos[0]:
@@ -77,7 +91,6 @@ class Slider(pygame.sprite.Sprite):
                 if self.rect.y + self.button.rect.y < mos_pos[1]:
                     if mos_pos[1] < self.rect.y + self.button.rect.y + self.button.rect.height:
                         self.selected = True
-                        print('clicked')
 
 
 if __name__ == '__main__':
